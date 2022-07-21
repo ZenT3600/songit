7e75a97b1de11e3c55f4957c2c3313f184406b23
ZenT3600 <matteo.leggio@tiscali.it>
2022-07-21 08:00:14 +0200

    Improved Performance By Using Threads ⚡️

--- songit.sh
+++ songit.sh
@@ -7,12 +7,37 @@ function generate-metadata() {
 	echo "> Recreating directory tree..." | glow -
 	find "$SOURCE" -type d -not -name "$REPO_DIR" -not -wholename "$REPO_PATH/*" -exec echo -e {} \; | sed "s@$SOURCE@@" | xargs -I{} mkdir "$REPO_PATH{}" 2> /dev/null
 	echo "> Extracting metadata... *(this may take a while*)" | glow -
-	find "$SOURCE" -type f -not -name "$REPO_DIR" -not -wholename "$REPO_PATH/*" -exec echo -e {} \; | sed "s@$SOURCE@@" | xargs -I{} bash -c "exiftool \"$(realpath $SOURCE)/{}\" > \"$REPO_PATH/{}.txt\""
+	readarray -d "" FL < <(find "$SOURCE" -maxdepth 1 -not -name "$REPO_DIR" -not -wholename "$SOURCE" -print0)
+	CS=4
+	for((i=0; i < ${#FL[@]}; i+=CS))
+	do
+		CHUNK=("${FL[@]:i:CS}")
+		L=${#CHUNK[@]}
+		ML=$(( L - 1 ))
+		for((j=0; j < $ML; j++))
+		do
+			find "${CHUNK[$j]}" -type f -not -name "$REPO_DIR" -not -wholename "$REPO_PATH/*" -exec echo -e {} \; | sed "s@$SOURCE@@" | xargs -I{} bash -c "exiftool \"$(realpath $SOURCE)/{}\" > \"$REPO_PATH/{}.txt\"" &
+			echo "Added ${CHUNK[$j]} to working queue"
+		done
+		find "${CHUNK[$ML]}" -type f -not -name "$REPO_DIR" -not -wholename "$REPO_PATH/*" -exec echo -e {} \; | sed "s@$SOURCE@@" | xargs -I{} bash -c "exiftool \"$(realpath $SOURCE)/{}\" > \"$REPO_PATH/{}.txt\""
+		echo "Added ${CHUNK[$ML]} to working queue"
+	done
+	while true
+	do
+		SFC=$(find "$SOURCE" -type f -not -path "$REPO_PATH/*" -not -wholename "$REPO_PATH" -printf "%f\n" | sed 's/\.txt//g')
+		RFC=$(find "$REPO_PATH" -type f -not -path "$REPO_PATH/.git/*" -not -wholename ".git" -printf "%f\n" | sed 's/\.txt//g')
+		if [[ "$RFC" == "$SFC" ]]
+		then
+			break
+		fi
+	done
 }
 
 function update-repo() {
 	test ! -d "$REPO_PATH/.git" && git -C "$REPO_PATH" init && git config --global --add safe.directory "$REPO_PATH"
 	echo "# Updating internal repo..." | glow -
+	date > "$REPO_PATH/lastupdate"
+	cp "$REPO_PATH/lastupdate" "$SOURCE/"
 	git -C "$REPO_PATH" add . -v
 	git -C "$REPO_PATH" commit -m "$(date)" -v
 }
@@ -23,17 +48,17 @@ function copy-over() {
 	echo "> Recreating directory tree..." | glow -
 	find "$SOURCE" -type d -not -name "$REPO_DIR" -not -wholename "$REPO_PATH/*" -exec echo -e {} \; | sed "s@$SOURCE@@" | xargs -I{} mkdir "$(realpath $TARGET)/{}" 2> /dev/null
 	test ! -d "$REPO_TARGET_PATH" && mkdir "$REPO_TARGET_PATH"
-	test ! -d "$REPO_TARGET_PATH/.git" && git -C "$REPO_TARGET_PATH" init && git config --global --add safe.directory "$REPO_TARGET_PATH" && git -C "$REPO_TARGET_PATH" config --local receive.denyCurrentBranch ignore
+	test ! -d "$REPO_TARGET_PATH/.git" && git -C "$REPO_TARGET_PATH" init && git config --global --add safe.directory "$REPO_TARGET_PATH" && git -C "$REPO_TARGET_PATH" config --local receive.denyCurrentBranch ignore && git -C "$REPO_TARGET_PATH" config --local pull.rebase false
 	echo "> Pushing internal repo changes" | glow -
 	git -C "$REPO_TARGET_PATH" remote add sourcerepo "$REPO_PATH"
 	git -C "$REPO_TARGET_PATH" remote update
 	git -C "$REPO_TARGET_PATH" fetch sourcerepo
 	git -C "$REPO_TARGET_PATH" pull sourcerepo master
-	git -C "$REPO_TARGET_PATH" remote rm sourcerepo
 	echo "> Copying changes from source" | glow -
+	set -e
 	git -C "$REPO_TARGET_PATH" log -n 1 --name-status --pretty="" | xargs -I{} echo -e "{}\n" | grep -i "^D" | awk '{$1 = ""; print $0}' | sed 's/\.txt//g' | sed 's/"//g' | xargs -I{} rm -rfv "$(realpath $TARGET)/{}"
-	git -C "$REPO_TARGET_PATH" log -n 1 --name-status --pretty="" | xargs -I{} echo -e "{}\n" | grep -i "^A" | awk '{$1 = ""; print $0}' | sed 's/\.txt//g' | sed 's/"//g' | xargs -I{} cp -v "$(realpath $SOURCE)/{}" "$(realpath $TARGET)/{}"
-	git -C "$REPO_TARGET_PATH" log -n 1 --name-status --pretty="" | xargs -I{} echo -e "{}\n" | grep -i "^M" | awk '{$1 = ""; print $0}' | sed 's/\.txt//g' | sed 's/"//g' | xargs -I{} cp -v "$(realpath $SOURCE)/{}" "$(realpath $TARGET)/{}"
+	git -C "$REPO_TARGET_PATH" log -n 1 --name-status --pretty="" | xargs -I{} echo -e "{}\n" | grep -i "^A" | awk '{$1 = ""; print $0}' | sed 's/\.txt//g' | sed 's/"//g' | xargs -I{} bash -c "cp -v \"$(realpath $SOURCE)/{}\" \"$(realpath $TARGET)/{}\""
+	git -C "$REPO_TARGET_PATH" log -n 1 --name-status --pretty="" | xargs -I{} echo -e "{}\n" | grep -i "^M" | awk '{$1 = ""; print $0}' | sed 's/\.txt//g' | sed 's/"//g' | xargs -I{} bash -c "cp -v \"$(realpath $SOURCE)/{}\" \"$(realpath $TARGET)/{}\""
 	find "$TARGET" -type d -empty -delete
 	#cp -vr "$REPO_PATH/"* "$REPO_TARGET_PATH"
 	#git -C "$REPO_TARGET_PATH" add .
