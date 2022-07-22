1966b964f4970c5593c89b5ad431aac6b6206eee
ZenT3600 <matteo.leggio@tiscali.it>
2022-07-22 06:40:10 +0200

    Removed glow Dependency ➖

--- songit.sh
+++ songit.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 
 function generate-metadata() {
+	echo "> Removing outdated metadata..."
 	rm -rf "$REPO_PATH/"*
 	test ! -d "$REPO_PATH" && mkdir "$REPO_PATH"
-	echo "# Generating metadata..." | glow -
-	echo "> Recreating directory tree..." | glow -
+	echo "Generating metadata..."
+	echo "> Recreating directory tree..."
 	find "$SOURCE" -type d -not -name "$REPO_DIR" -not -wholename "$REPO_PATH/*" -exec echo -e {} \; | sed "s@$SOURCE@@" | xargs -I{} mkdir "$REPO_PATH{}" 2> /dev/null
-	echo "> Extracting metadata... *(this may take a while*)" | glow -
+	echo "> Extracting metadata... (this may take a while)"
 	readarray -d "" FL < <(find "$SOURCE" -maxdepth 1 -not -name "$REPO_DIR" -not -wholename "$SOURCE" -print0)
 	CS=4
 	for((i=0; i < ${#FL[@]}; i+=CS))
@@ -17,16 +18,18 @@ function generate-metadata() {
 		for((j=0; j < $ML; j++))
 		do
 			find "${CHUNK[$j]}" -type f -not -name "$REPO_DIR" -not -wholename "$REPO_PATH/*" -exec echo -e {} \; | sed "s@$SOURCE@@" | xargs -I{} bash -c "exiftool \"$(realpath $SOURCE)/{}\" > \"$REPO_PATH/{}.txt\"" &
-			echo "Added ${CHUNK[$j]} to working queue"
+			echo "> > Added ${CHUNK[$j]} to working queue"
 		done
 		find "${CHUNK[$ML]}" -type f -not -name "$REPO_DIR" -not -wholename "$REPO_PATH/*" -exec echo -e {} \; | sed "s@$SOURCE@@" | xargs -I{} bash -c "exiftool \"$(realpath $SOURCE)/{}\" > \"$REPO_PATH/{}.txt\""
-		echo "Added ${CHUNK[$ML]} to working queue"
+		echo "> > Added ${CHUNK[$ML]} to working queue"
 	done
+	echo "> > Working..."
 	while true
 	do
-		SFC=$(find "$SOURCE" -type f -not -path "$REPO_PATH/*" -not -wholename "$REPO_PATH" -printf "%f\n" | sed 's/\.txt//g')
+		SFC=$(find "$SOURCE" -type f -not -path "$REPO_PATH/*" -not -wholename "$REPO_PATH" -printf "%f\n")
 		RFC=$(find "$REPO_PATH" -type f -not -path "$REPO_PATH/.git/*" -not -wholename ".git" -printf "%f\n" | sed 's/\.txt//g')
-		if [[ "$RFC" == "$SFC" ]]
+
+		if [[ "$(echo $RFC | wc -l)" -eq "$(echo $SFC | wc -l)" ]]
 		then
 			break
 		fi
@@ -35,7 +38,7 @@ function generate-metadata() {
 
 function update-repo() {
 	test ! -d "$REPO_PATH/.git" && git -C "$REPO_PATH" init && git config --global --add safe.directory "$REPO_PATH"
-	echo "# Updating internal repo..." | glow -
+	echo "Updating internal repo..."
 	date > "$REPO_PATH/lastupdate"
 	cp "$REPO_PATH/lastupdate" "$SOURCE/"
 	git -C "$REPO_PATH" add . -v
@@ -44,17 +47,17 @@ function update-repo() {
 
 function copy-over() {
 	GIT_DISCOVERY_ACROSS_FILESYSTEM=1
-	echo "# Copying over data..." | glow -
-	echo "> Recreating directory tree..." | glow -
+	echo "Copying over data..."
+	echo "> Recreating directory tree..."
 	find "$SOURCE" -type d -not -name "$REPO_DIR" -not -wholename "$REPO_PATH/*" -exec echo -e {} \; | sed "s@$SOURCE@@" | xargs -I{} mkdir "$(realpath $TARGET)/{}" 2> /dev/null
 	test ! -d "$REPO_TARGET_PATH" && mkdir "$REPO_TARGET_PATH"
 	test ! -d "$REPO_TARGET_PATH/.git" && git -C "$REPO_TARGET_PATH" init && git config --global --add safe.directory "$REPO_TARGET_PATH" && git -C "$REPO_TARGET_PATH" config --local receive.denyCurrentBranch ignore && git -C "$REPO_TARGET_PATH" config --local pull.rebase false
-	echo "> Pushing internal repo changes" | glow -
+	echo "> Pushing internal repo changes"
 	git -C "$REPO_TARGET_PATH" remote add sourcerepo "$REPO_PATH"
 	git -C "$REPO_TARGET_PATH" remote update
 	git -C "$REPO_TARGET_PATH" fetch sourcerepo
 	git -C "$REPO_TARGET_PATH" pull sourcerepo master
-	echo "> Copying changes from source" | glow -
+	echo "> Copying changes from source"
 	set -e
 	git -C "$REPO_TARGET_PATH" log -n 1 --name-status --pretty="" | xargs -I{} echo -e "{}\n" | grep -i "^D" | awk '{$1 = ""; print $0}' | sed 's/\.txt//g' | sed 's/"//g' | xargs -I{} rm -rfv "$(realpath $TARGET)/{}"
 	git -C "$REPO_TARGET_PATH" log -n 1 --name-status --pretty="" | xargs -I{} echo -e "{}\n" | grep -i "^A" | awk '{$1 = ""; print $0}' | sed 's/\.txt//g' | sed 's/"//g' | xargs -I{} bash -c "cp -v \"$(realpath $SOURCE)/{}\" \"$(realpath $TARGET)/{}\""
