b8dd80ec3bb28b0d5b755d71f461c055490f984c
ZenT3600 <matteo.leggio@tiscali.it>
2022-07-24 16:08:34 +0200

    Performed Tests and Changed Accordingly ✅

--- songit.sh
+++ songit.sh
@@ -2,15 +2,16 @@
 
 function generate-metadata() {
 	echo "> Removing outdated metadata..."
-	rm -rf "$REPO_PATH/"*
 	test ! -d "$REPO_PATH" && mkdir "$REPO_PATH"
+	find "$REPO_PATH" -type f -not -path "*/.git/*" -exec echo -e {} \; | sed 's/\.txt//g' | sed "s@$REPO_PATH@@" | xargs -I{} bash -c "echo \"> > Looking at Directory \$(dirname \"{}\")...\" && if [ ! -f \"$(realpath "$SOURCE"){}\" ]; then echo > \"$(realpath "$REPO_PATH"){}.txt\"; else (( \$(date -r \"$(realpath "$SOURCE"){}\" +%s) >= \$(date -r \"$(realpath "$REPO_PATH"){}.txt\" +%s) )) && echo > \"$(realpath $SOURCE){}.txt\"; fi" | uniq
+	find "$REPO_PATH" -type d -empty -delete
 	echo "Generating metadata..."
 	echo "> Recreating directory tree..."
 	find "$SOURCE" -type d -not -name "$REPO_DIR" -not -wholename "$REPO_PATH/*" -exec echo -e {} \; | sed "s@$SOURCE@@" | xargs -I{} mkdir "$REPO_PATH/{}" 2> /dev/null
 	echo "> Extracting metadata... (this may take a while)"
-	find "$SOURCE" -maxdepth 1 -not -path "*/$REPO_DIR/*" -not -wholename "$SOURCE" -not -name "$REPO_DIR" -exec echo -e {} \; | while read f; do
-		find "$f" -type f -not -name "$REPO_DIR" -not -wholename "$REPO_PATH/*" -exec echo -e {} \; | sed "s@$SOURCE@@" | xargs -I{} bash -c "exiftool \"$(realpath $SOURCE)/{}\" > \"$(realpath $REPO_PATH)/{}.txt\""
-		echo "> > Added $(basename "$f") to working queue"
+	find "$SOURCE" -maxdepth 1 -not -path "*/$REPO_DIR/*" -not -wholename "$SOURCE" -not -name "$REPO_DIR" -exec echo -e {} \; | while read -r d; do
+	find "$d" -type f -not -name "$REPO_DIR" -not -wholename "$REPO_PATH/*" -exec echo -e {} \; | sed "s@$SOURCE@@" | xargs -I{} bash -c "if [ ! -s \"$(realpath $REPO_PATH){}.txt\" ]; then echo \"> > Regenerating \$(basename \"{}\")\"; exiftool \"$(realpath "$SOURCE"){}\" > \"$(realpath "$REPO_PATH"){}.txt\"; fi"
+		echo "> > Added $(basename "$d") to working queue"
 	done
 }
 
@@ -24,22 +25,22 @@ function update-repo() {
 }
 
 function copy-over() {
-	GIT_DISCOVERY_ACROSS_FILESYSTEM=1
+	export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 	echo "Copying over data..."
 	echo "> Recreating directory tree..."
-	find "$SOURCE" -type d -not -name "$REPO_DIR" -not -wholename "$REPO_PATH/*" -exec echo -e {} \; | sed "s@$SOURCE@@" | xargs -I{} mkdir "$(realpath $TARGET)/{}" 2> /dev/null
+	find "$SOURCE" -type d -not -name "$REPO_DIR" -not -wholename "$REPO_PATH/*" -exec echo -e {} \; | sed "s@$SOURCE@@" | xargs -I{} mkdir "$(realpath "$TARGET")/{}" 2> /dev/null
 	test ! -d "$REPO_TARGET_PATH" && mkdir "$REPO_TARGET_PATH"
-	test ! -d "$REPO_TARGET_PATH/.git" && git -C "$REPO_TARGET_PATH" init && git config --global --add safe.directory "$REPO_TARGET_PATH" && git -C "$REPO_TARGET_PATH" config --local receive.denyCurrentBranch ignore && git -C "$REPO_TARGET_PATH" config --local pull.rebase false
+	test ! -d "$REPO_TARGET_PATH/.git" && git -C "$REPO_TARGET_PATH" init && git config --global --add safe.directory "$REPO_TARGET_PATH" && git -C "$REPO_TARGET_PATH" config --local receive.denyCurrentBranch ignore && git -C "$REPO_TARGET_PATH" config pull.rebase false
 	echo "> Pushing internal repo changes"
 	git -C "$REPO_TARGET_PATH" remote add sourcerepo "$REPO_PATH"
 	git -C "$REPO_TARGET_PATH" remote update
 	git -C "$REPO_TARGET_PATH" fetch sourcerepo
-	git -C "$REPO_TARGET_PATH" pull sourcerepo master
+	git -C "$REPO_TARGET_PATH" pull --no-rebase sourcerepo master
 	echo "> Copying changes from source"
 	set -e
-	git -C "$REPO_TARGET_PATH" log -n 1 --name-status --pretty="" | xargs -I{} echo -e "{}\n" | grep -i "^D" | awk '{$1 = ""; print $0}' | sed 's/\.txt//g' | sed 's/"//g' | xargs -I{} rm -rfv "$(realpath $TARGET)/{}"
-	git -C "$REPO_TARGET_PATH" log -n 1 --name-status --pretty="" | xargs -I{} echo -e "{}\n" | grep -i "^A" | awk '{$1 = ""; print $0}' | sed 's/\.txt//g' | sed 's/"//g' | xargs -I{} bash -c "cp -v \"$(realpath $SOURCE)/{}\" \"$(realpath $TARGET)/{}\""
-	git -C "$REPO_TARGET_PATH" log -n 1 --name-status --pretty="" | xargs -I{} echo -e "{}\n" | grep -i "^M" | awk '{$1 = ""; print $0}' | sed 's/\.txt//g' | sed 's/"//g' | xargs -I{} bash -c "cp -v \"$(realpath $SOURCE)/{}\" \"$(realpath $TARGET)/{}\""
+	git -C "$REPO_TARGET_PATH" log -n 1 --name-status --pretty="" | xargs -I{} echo -e "{}\n" | grep -i "^D" | awk '{$1 = ""; print $0}' | sed 's/\.txt//g' | sed 's/"//g' | xargs -I{} rm -rfv "$(realpath "$TARGET")/{}"
+	git -C "$REPO_TARGET_PATH" log -n 1 --name-status --pretty="" | xargs -I{} echo -e "{}\n" | grep -i "^A" | awk '{$1 = ""; print $0}' | sed 's/\.txt//g' | sed 's/"//g' | xargs -I{} bash -c "cp -v \"$(realpath "$SOURCE")/{}\" \"$(realpath "$TARGET")/{}\""
+	git -C "$REPO_TARGET_PATH" log -n 1 --name-status --pretty="" | xargs -I{} echo -e "{}\n" | grep -i "^M" | awk '{$1 = ""; print $0}' | sed 's/\.txt//g' | sed 's/"//g' | xargs -I{} bash -c "cp -v \"$(realpath "$SOURCE")/{}\" \"$(realpath "$TARGET")/{}\""
 	find "$TARGET" -type d -empty -delete
 	#cp -vr "$REPO_PATH/"* "$REPO_TARGET_PATH"
 	#git -C "$REPO_TARGET_PATH" add .
@@ -47,19 +48,19 @@ function copy-over() {
 }
 
 SOURCE=$1
-test -z $SOURCE && exit
+test -z "$SOURCE" && exit
 
 TARGET=$2
-test -z $TARGET && exit
+test -z "$TARGET" && exit
 
 if [ -f .env ]; then
-	export $(cat .env | xargs)
+	export "$(cat .env | xargs)"
 else
 	REPO_DIR=".songit"
 fi
 
-REPO_PATH=$(realpath $SOURCE)/$REPO_DIR
-REPO_TARGET_PATH=$(realpath $TARGET)/$REPO_DIR
+REPO_PATH=$(realpath "$SOURCE")/$REPO_DIR
+REPO_TARGET_PATH=$(realpath "$TARGET")/$REPO_DIR
 
 generate-metadata
 update-repo
